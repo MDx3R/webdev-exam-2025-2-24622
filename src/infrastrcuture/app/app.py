719f37530bb98b5d62f8dc581ca5f76c9b5f1a3f
MDx3R <nationmdxr@gmail.com>
2025-06-18 22:54:33 +0300

    add app for run

---
+++ src/infrastrcuture/app/app.py
@@ -0,0 +1,27 @@
+from application.transactions.configuration import CurrentTransactionManager
+from di.container import Container
+from infrastrcuture.server.flask.app import FlaskServer
+from infrastrcuture.sqlalchemy.database import Database
+
+
+class App:
+    def __init__(self) -> None:
+        self.container = Container()
+        self.container.init_resources()
+        self.config = self.container.config()
+
+        self.database = Database(self.config.DB)
+        self.server = FlaskServer(self.container)
+
+    def configure(self):
+        CurrentTransactionManager.set(self.container.transaction_manager())
+
+        self.server.configure()
+        self.server.setup_routes()
+
+    def run(self):
+        self.server.app.run()
+        self.database.shutdown()
+
+    def get_server(self):
+        return self.server.app
---
+++ src/run/flask/main.py
@@ -0,0 +1,7 @@
+from infrastrcuture.app.app import App
+
+
+if __name__ == "__main__":
+    application = App()
+    application.configure()
+    application.run()
